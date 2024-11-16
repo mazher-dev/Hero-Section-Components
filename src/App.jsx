f97b6e366513ac30d9e7033ed17ce76c321cf68b
MH Code <mazheriftikhar1@gmail.com>
2024-11-16 16:49:43 +0500

    16-Nov changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Components/Hero1/Hero'
 import Hero2 from './Components/Hero2/Hero2'
 import Hero3 from './Components/Hero3/Hero3'
 import Hero4 from './Components/Hero4/Hero4'
+import Hero5 from './Hero5/Hero5'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero2 />
       <Hero3 />
       <Hero4 />
+      <Hero5 />
     </div>
   )
 }
