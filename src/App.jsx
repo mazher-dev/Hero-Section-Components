98cafa1844605844727f5826b9dbd82977d0116b
MH Code <mazheriftikhar1@gmail.com>
2024-11-16 15:14:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './Components/Hero1/Hero'
+import Hero2 from './Components/Hero2/Hero2'
+import Hero3 from './Components/Hero3/Hero3'
+import Hero4 from './Components/Hero4/Hero4'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <Hero2 />
+      <Hero3 />
+      <Hero4 />
+    </div>
+  )
+}
+
+export default App
